2c622186adda58ad31cb4c7ccf771471edde45f6
AymanSLH <ayman171salah@gmail.com>
2018-06-28 19:29:04 +0200

    removed media files

--- videolesson.php
+++ videolesson.php
@@ -3,6 +3,7 @@
 <title>Eldad Lesson</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/w3.css">
+<link rel="stylesheet" href="css/navbar.css">
 <link rel="stylesheet" href="css/video.css">
 <link rel="icon" href="img/rocket.ico">
 <body>
@@ -19,7 +20,61 @@
 <div style="margin-left:25%">
 
     <div class="w3-container w3-teal">
-        <h1>My Page</h1>
+<!--        <h1>My Page</h1>-->
+
+        <main>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="">Forum</a></li>
+                    <li><a href="#">Chat</a></li>
+
+
+                    <li><a href="<?php
+                        if(isset($_COOKIE['logincookie']))
+                        {
+                            echo "" ;
+                        }
+                        else
+                        {
+                            echo "login.php" ;
+                        }
+                        ?>"><?php
+                            if(isset($_COOKIE['logincookie']))
+                            {
+                                echo "Welcome !" ;
+                            }
+                            else
+                            {
+                                echo "Log in " ;
+                            }
+                            ?></a></li>
+
+
+
+                    <li><a href="<?php
+                        if(isset($_COOKIE['logincookie']))
+                        {
+                            echo "profile.php" ;
+                        }
+                        else
+                        {
+                            echo "signup.html" ;
+                        }
+                        ?>"><?php
+                            if(isset($_COOKIE['logincookie']))
+                            {
+                                echo $_COOKIE['loginname'] ;
+                            }
+                            else
+                            {
+                                echo "Sign Up" ;
+                            }
+                            ?></a></li>
+                </ul>
+            </nav>
+        </main>
+
     </div>
 
     <video src="videos/momo.mp4"  style="width:100%;height:450px" controls>
