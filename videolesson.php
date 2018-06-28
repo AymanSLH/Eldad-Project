2beb70f8bfca9540b9d021204a3dfcbb106801d2
AymanSLH <ayman171salah@gmail.com>
2018-06-28 19:20:08 +0200

    Added Video page and Style

---
+++ videolesson.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<title>Eldad Lesson</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/w3.css">
+<link rel="stylesheet" href="css/video.css">
+<link rel="icon" href="img/rocket.ico">
+<body>
+
+<!-- Sidebar -->
+<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
+    <h3 class="w3-bar-item">Menu</h3>
+    <a href="#" class="w3-bar-item w3-button">Link 1</a>
+    <a href="#" class="w3-bar-item w3-button">Link 2</a>
+    <a href="#" class="w3-bar-item w3-button">Link 3</a>
+</div>
+
+<!-- Page Content -->
+<div style="margin-left:25%">
+
+    <div class="w3-container w3-teal">
+        <h1>My Page</h1>
+    </div>
+
+    <video src="videos/momo.mp4"  style="width:100%;height:450px" controls>
+    </video>
+    <div class="w3-container">
+       <h3>Rate This Lesson</h3>
+        <div class="rating">
+            <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
